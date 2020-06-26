77d4ad15e89363f83aabdc323e922da5e6053f27
Nabeel529886 <48175387+Nabeel529886@users.noreply.github.com>
2020-06-26 14:52:00 +0500

    empty transaction restriction and timestamp correction

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -4,6 +4,7 @@ import {GlobalContext} from '../context/GlobalState'
 export const AddTransaction = () => {
     const [text, setText] = useState('')
     const [amount, setAmount] = useState(0)
+    const transactionTimeStamp = new Date()
 
     const {addIncome, addExpense} = useContext(GlobalContext)
 
@@ -16,13 +17,15 @@ export const AddTransaction = () => {
     const income = {
         id: Math.floor(Math.random() * 10000),
         text,
-        amount: +amount
+        amount: +amount,
+        timeStamp: transactionTimeStamp.toLocaleString()
     }
 
     const expense = {
         id: Math.floor(Math.random() * 10000),
         text,
-        amount: -amount
+        amount: -amount,
+        timeStamp: transactionTimeStamp.toLocaleString()
     }
 
 
@@ -33,8 +36,12 @@ export const AddTransaction = () => {
                 <input id="text" type="text" value={text} onChange={(e) => setText(e.target.value)}/>
                 <label className="amount" htmlFor="amount">Amount (Income/Expense)</label>
                 <input id="amount" type="number" value={amount} onChange={(e) => setAmount(e.target.value)} />
-                <button className="income-btn" onClick={() => addIncome(income)}>Income</button>
-                <button className="expense-btn" onClick={() => addExpense(expense)}>Expense</button>
+                <button className="income-btn" onClick={() => (
+                  income.amount === 0?alert("Cannot add income of $0"):addIncome(income)  
+                )}>Income</button>
+                <button className="expense-btn" onClick={() => (
+                  expense.amount === 0?alert("Cannot add expense of $0"):addExpense(expense)  
+                )}>Expense</button>
             </form>
         </div>
     )
--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -4,13 +4,12 @@ import {GlobalContext} from '../context/GlobalState'
 
 export const Transaction = ({ transaction }) => {
     const {deleteTransaction} = useContext(GlobalContext)
-    const currentDate = new Date()
 
     return (
         <div>
             <li className={transaction.amount > 0? "item-plus": "item-minus"}>
-            {transaction.text}<small>{transaction.amount>0? "Income added on ": "Expense added on "}{currentDate.toLocaleString()}</small>
-                <span>${transaction.amount}</span>
+            {transaction.text}<small>{transaction.amount>0? "Income added on ": "Expense added on "}{transaction.timeStamp}</small>
+                    <span>${transaction.amount}</span>
                 <button onClick={() => deleteTransaction(transaction.id)}>X</button>
             </li>
         </div>
